f61bb5eb59ba78ed9e46738ce96b0fefded08071
Matt Stratton <matt.stratton@gmail.com>
2016-12-02 16:08:09 -0600

    Clean up functional test
    Signed-off-by: Matt Stratton <matt.stratton@gmail.com>

--- recipes/functional.rb
+++ recipes/functional.rb
@@ -19,41 +19,41 @@
 include_recipe 'delivery-truck::functional'
 
 with_server_config do
-    inspec_data = get_project_secrets
+  inspec_data = get_project_secrets
 
-    # Search to get nodes
-    search_query = "recipes:#{node['delivery']['change']['project']}* AND chef_environment:#{delivery_environment}"
-    nodes = search('node', search_query.to_s)
+  # Search to get nodes
+  search_query = "recipes:#{node['delivery']['change']['project']}* AND " \
+  "chef_environment:#{delivery_environment}"
+  nodes = search('node', search_query.to_s)
 
-    case node['os']
+  nodes.each do |infra_node|
+    case infra_node['os']
     when 'linux'
-        ssh_user = inspec_data['inspec']['ssh-user']
-        ssh_key_path = "#{workflow_workspace}/#{inspec_data['inspec']['ssh-user']}.pem"
-
-        nodes.each do |infra_node|
-            # Execute inspec
-            execute 'execute_functional_inspec' do
-                command '/opt/chefdk/embedded/bin/inspec ' \
-                        "exec #{node['delivery']['workspace']['repo']}/test/recipes/ " \
-                        "-t ssh://#{ssh_user}@#{infra_node['ipaddress']} " \
-                        "-i #{ssh_key_path}"
-                action :run
-                live_stream true
-            end
-        end
+      ssh_user = inspec_data['inspec']['ssh-user']
+      ssh_key_path = "#{workflow_workspace}/"\
+      "#{inspec_data['inspec']['ssh-user']}.pem"
+      # Execute inspec
+      execute 'execute_functional_inspec' do
+        command '/opt/chefdk/embedded/bin/inspec ' \
+                  "exec #{node['delivery']['workspace']['repo']}/" \
+                  'test/recipes/ ' \
+                  "-t ssh://#{ssh_user}@#{infra_node['ipaddress']} " \
+                  "-i #{ssh_key_path}"
+        action :run
+        live_stream true
+      end
     when 'windows'
       winrm_user = inspec_data['inspec']['winrm-user']
       winrm_password = inspec_data['inspec']['winrm-password']
-        nodes.each do |infra_node|
-            # Execute inspec
-            execute 'execute_functional_inspec' do
-                command '/opt/chefdk/embedded/bin/inspec ' \
-                        "exec #{node['delivery']['workspace']['repo']}/test/recipes/ " \
-                        "-t winrm://#{winrm_user}@#{infra_node['ipaddress']} " \
-                        "--password '#{winrm_password}'"
-                action :run
-                live_stream true
-            end
-        end
+      execute 'execute_functional_inspec' do
+        command '/opt/chefdk/embedded/bin/inspec ' \
+                  "exec #{node['delivery']['workspace']['repo']}"\
+                  '/test/recipes/ ' \
+                  "-t winrm://#{winrm_user}@#{infra_node['ipaddress']} " \
+                  "--password '#{winrm_password}'"
+        action :run
+        live_stream true
+      end
+    end
   end
 end
