ac1e93fab5a20e9d6ae0824c6361d84c4a5df742
Matt Stratton <matt.stratton@gmail.com>
2016-12-05 08:16:13 -0600

    Use a create file for winrm script
    Signed-off-by: Matt Stratton <matt.stratton@gmail.com>

--- recipes/functional.rb
+++ recipes/functional.rb
@@ -45,6 +45,9 @@ with_server_config do
     when 'windows'
       winrm_user = inspec_data['inspec']['winrm-user']
       winrm_password = inspec_data['inspec']['winrm-password']
+      file "#{workflow_workspace_repo}/#{workflow_phase}/#{workflow_stage}/repo/inspec-winrm.sh" do
+        content "Hello"
+      end
       execute 'execute_functional_inspec' do
         command '/opt/chefdk/embedded/bin/inspec ' \
                   "exec #{node['delivery']['workspace']['repo']}"\
